ae87c0374589b9116deeee9fb9e3137a4242d233
Makshoof Kamal Jilani <140198597+makshoof@users.noreply.github.com>
2025-02-11 00:59:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
 import streamlit as st
 import numpy as np
 import pickle
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequences import pad_sequences
+from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 
-model = load_model ("next_word_lstm.h5")
 
-with open("tokenizer.pkl",'rb') as handle:
-    tokenizer=pickle.load(handle)
+model = load_model('next_word_lstm.h5')
+
+with open('tokenizer.pkl', 'rb') as file:
+    tokenizer = pickle.load(file)
 
 
 def predict_next_word(model, tokenizer, text, max_sequence_len):
-    token_list = tokenizer.texts_to_sequences([text])[0]
-    token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
-    predicted = model.predict(token_list, verbose=0)
-    predicted_index = np.argmax(predicted)
+    sequence = tokenizer.texts_to_sequences([text])
+    sequence = pad_sequences(sequence, maxlen=max_sequence_len-1, padding='pre')
+    predicted = np.argmax(model.predict(sequence), axis=-1)
+    
     for word, index in tokenizer.word_index.items():
-        if index == predicted_index:
+        if index == predicted:
             return word
-    return None  # Return None if word not found in tokenizer
+    return ""
 
-st.title("Next word predict")
-input_text=st.text_input("Enetr the sequence of word", "The sun is shining ")
-if st.button("Predict: Next word"):
-    max_sequences_len = model.input_shape[1]+1
-    next_word = predict_next_word(model,tokenizer,input_text,max_sequences_len)
-    st.write('the predicition next word is: ',next_word) 
+
+st.title("Next Word Predictor")
+st.write("Enter the phrase")
+
+
+user_input = st.text_input("Enter your text:")
+
+
+max_sequence_len = model.input_shape[1]
+
+if st.button("Predict Next Word"):
+    if user_input:
+        next_word = predict_next_word(model, tokenizer, user_input, max_sequence_len)
+        if next_word:
+            st.success(f"Predicted next word: *{next_word}*")
+        else:
+            st.warning("Unable to predict, Try something different")
+    else:
+        st.error("Enter some text to predict the next word.")
